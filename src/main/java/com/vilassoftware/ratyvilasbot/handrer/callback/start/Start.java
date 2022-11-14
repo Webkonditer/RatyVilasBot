a45f08f99c6b6bb73e21d63ccb008eb320086bab
6564747347456845 <webkonditer@yandex.ru>
2022-11-14 21:35:22 +0300

    phone button

--- src/main/java/com/vilassoftware/ratyvilasbot/handrer/callback/start/Start.java
+++ src/main/java/com/vilassoftware/ratyvilasbot/handrer/callback/start/Start.java
@@ -4,7 +4,14 @@ import com.vilassoftware.ratyvilasbot.service.SendingMessages;
 import com.vilassoftware.ratyvilasbot.service.UserManagement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -43,9 +50,39 @@ public class Start {
      */
     public void startCommandReceived(long chatId, String firstName) {
 
-        String message = GREETING + firstName + "! " + WELCOME_MESSAGE;
-        sendingMessages.sendMessageWithMenu(chatId, message);
-        log.info("A welcome message has been sent to the user " + firstName + ", Id: " + chatId);
+//        String message = GREETING + firstName + "! " + WELCOME_MESSAGE;
+//        sendingMessages.sendMessageWithMenu(chatId, message);
+//        log.info("A welcome message has been sent to the user " + firstName + ", Id: " + chatId);
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("You send /start");
+
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        KeyboardButton keyboardButton = new KeyboardButton();
+
+        keyboardButton.setText("Share your number >");
+        keyboardButton.setRequestContact(true);
+        keyboardFirstRow.add(keyboardButton);
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+        sendingMessages.executeMessage(sendMessage);
 
     }
 
